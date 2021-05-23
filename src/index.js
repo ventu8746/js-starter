9f66c814bfa45e48e03ada577fb6c9d62120c4aa
Ventu4687 <ventu87@hotmail.it>
2021-05-23 18:47:58 +0200

    changes index.html

--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
 import { html, render } from "lit-html";
 
 const app = html`
-	<body>
-		<div class="container">
-			<header>
-				<h1>App Todo List</h1>
-				<input type="text" name="todo-elemnt" id="todo-elemnt" />
-				<button>Aggiungi</button>
-			</header>
-			<main>
-				<div class="list-todo">
-					<ul>
-						<li>Comprare Pane <button>Rimuovi</button></li>
+	<div class="container">
+		<header>
+			<h1>App Todo List</h1>
+			<input type="text" name="todo-elemnt" id="todo-elemnt" />
+			<button>Aggiungi</button>
+		</header>
+		<main>
+			<div class="list-todo">
+				<ul>
+					<li>Comprare Pane <button>Rimuovi</button></li>
 
-						<br />
+					<br />
 
-						<li>Comprare Latte <button>Rimuovi</button></li>
+					<li>Comprare Latte <button>Rimuovi</button></li>
 
-						<br />
-						<li>Comprare Vino <button>Rimuovi</button></li>
+					<br />
+					<li>Comprare Vino <button>Rimuovi</button></li>
 
-						<br />
-						<li>Comprare Uovafdsdssdfds<button>Rimuovi</button></li>
-					</ul>
-				</div>
-			</main>
-		</div>
-	</body>
+					<br />
+					<li>Comprare Uovafdsdssdfds<button>Rimuovi</button></li>
+				</ul>
+			</div>
+		</main>
+	</div>
 `;
 
 render(app, document.getElementById("root"));

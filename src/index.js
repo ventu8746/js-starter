73326d999fc41fd181d36d48f932874764fbcb27
Alessandro Scandone <alescandone@gmail.com>
2021-05-25 12:30:21 +0200

    added prettier config

--- src/index.js
+++ src/index.js
@@ -1,29 +1,3 @@
-import { html, render } from "lit-html";
 import { Model } from "./todo";
-
-function viewTodoItem(elem) {
-	return html`<li>Comprare ${elem} <button>Rimuovi</button></li>`;
-}
-
-const todos = ["ciao", "cereali", "calzetti"];
-
-//const items = todos.map((elem) => viewTodoItem(elem));
-
-// const app = html`
-//   <div class="container">
-//     <header>
-//       <h1>App Todo List</h1>
-//       <input type="text" name="todo-elemnt" id="todo-elemnt" />
-//       <button>Aggiungi</button>
-//     </header>
-//     <main>
-//       <div class="list-todo">
-//         <ul>
-//           ${items}
-//         </ul>
-//       </div>
-//     </main>
-//   </div>
-// `;
 
 const app = new Model(document.getElementById("root"));
--- src/todo.js
+++ src/todo.js
@@ -1,57 +1,57 @@
 import { html, render } from "lit-html";
 
 export class Model {
-	constructor(node) {
-		this.addItem("cereali");
-		this.addItem("vino");
-		this.addItem("pane");
-		render(this.view(), node);
-	}
+  constructor(node) {
+    this.addItem("cereali");
+    this.addItem("vino");
+    this.addItem("pane");
+    render(this.view(), node);
+  }
 
-	_items = [];
+  _items = [];
 
-	// Functionality
-	addItem(todo) {
-		this._items.push({ todo: todo, completed: false });
-	}
+  // Functionality
+  addItem(todo) {
+    this._items.push({ todo: todo, completed: false });
+  }
 
-	toogleItem(index) {
-		this._items[index].completed = !this._items[index].completed;
-	}
+  toogleItem(index) {
+    this._items[index].completed = !this._items[index].completed;
+  }
 
-	// View
-	view() {
-		return html`
-			<div class="container">
-				<header>
-					<h1>App Todo List</h1>
-					<input type="text" name="todo-elemnt" id="todo-elemnt" />
-					<button>Aggiungi</button>
-				</header>
-				<main>
-					<div class="list-todo">
-						<ul>
-							${this._items.map(
-								(obj) =>
-									html`<li>Comprare ${obj.todo} <button>Rimuovi</button></li>`
-							)}
-						</ul>
-					</div>
-				</main>
-			</div>
-		`;
-	}
+  // View
+  view() {
+    return html`
+      <div class="container">
+        <header>
+          <h1>App Todo List</h1>
+          <input type="text" name="todo-elemnt" id="todo-elemnt" />
+          <button>Aggiungi</button>
+        </header>
+        <main>
+          <div class="list-todo">
+            <ul>
+              ${this._items.map(
+                (obj) =>
+                  html`<li>Comprare ${obj.todo} <button>Rimuovi</button></li>`
+              )}
+            </ul>
+          </div>
+        </main>
+      </div>
+    `;
+  }
 
-	// Utils
-	logItems() {
-		let itemsProv = [];
-		for (let i = 0; i < this._items.length; i++) {
-			if (this._items[i].completed) {
-				itemsProv.push(`${i}. ${this._items[i]["todo"]} (fatto)`);
-			} else {
-				itemsProv.splice(i, 1, `${i}. ${this._items[i]["todo"]} `);
-			}
-		}
-		return itemsProv.join(" \n");
-	}
+  // Utils
+  logItems() {
+    let itemsProv = [];
+    for (let i = 0; i < this._items.length; i++) {
+      if (this._items[i].completed) {
+        itemsProv.push(`${i}. ${this._items[i]["todo"]} (fatto)`);
+      } else {
+        itemsProv.splice(i, 1, `${i}. ${this._items[i]["todo"]} `);
+      }
+    }
+    return itemsProv.join(" \n");
+  }
 }

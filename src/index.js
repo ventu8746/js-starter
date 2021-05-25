4b45a91a00123abb4b5c35ecde27836a89eb2794
Ventu4687 <ventu87@hotmail.it>
2021-05-25 11:42:59 +0200

    integration app todo

--- src/index.js
+++ src/index.js
@@ -2,28 +2,28 @@ import { html, render } from "lit-html";
 import { Model } from "./todo";
 
 function viewTodoItem(elem) {
-  return html`<li>Comprare ${elem} <button>Rimuovi</button></li>`;
+	return html`<li>Comprare ${elem} <button>Rimuovi</button></li>`;
 }
 
 const todos = ["ciao", "cereali", "calzetti"];
 
-const items = todos.map((elem) => viewTodoItem(elem));
+//const items = todos.map((elem) => viewTodoItem(elem));
 
-const app = html`
-  <div class="container">
-    <header>
-      <h1>App Todo List</h1>
-      <input type="text" name="todo-elemnt" id="todo-elemnt" />
-      <button>Aggiungi</button>
-    </header>
-    <main>
-      <div class="list-todo">
-        <ul>
-          ${items}
-        </ul>
-      </div>
-    </main>
-  </div>
-`;
+// const app = html`
+//   <div class="container">
+//     <header>
+//       <h1>App Todo List</h1>
+//       <input type="text" name="todo-elemnt" id="todo-elemnt" />
+//       <button>Aggiungi</button>
+//     </header>
+//     <main>
+//       <div class="list-todo">
+//         <ul>
+//           ${items}
+//         </ul>
+//       </div>
+//     </main>
+//   </div>
+// `;
 
 const app = new Model(document.getElementById("root"));
